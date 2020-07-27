439220d485fb75ecb689730a7fe662576cf6101c
Muhi Masri <muhi@muhimasri.com>
2020-07-26 20:01:29 -0400

    Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+const USER_INFO = [
+  {name: 'John Smith', dateOfBirth: '1984-05-05', age: 36},
+  {name: 'Muhi Masri', dateOfBirth: '1992-02-02', age: 28},
+  {name: 'Peter Adams',  dateOfBirth: '2000-01-01', age: 20},
+  {name: 'Lora Bay',  dateOfBirth: '1977-03-03', age: 43},
+];
+
+const USER_SCHEMA = {
+  name: 'text',
+  dateOfBirth: 'date',
+  age: 'number',
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-dynamic-table';
+  displayedColumns: string[] = ['name', 'dateOfBirth', 'age', '$$edit'];
+  dataSource = USER_INFO;
+  dataSchema = USER_SCHEMA;
+  isEdit = true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
