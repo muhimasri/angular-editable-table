e5aec7677e1ac8d2a90256db96b358b11d527b50
Muhi Masri <muhi@muhimasri.com>
2020-07-27 16:48:11 -0400

    Clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 
 const USER_INFO = [
-  {name: 'John Smith', dateOfBirth: '1984-05-05', age: 36},
-  {name: 'Muhi Masri', dateOfBirth: '1992-02-02', age: 28},
-  {name: 'Peter Adams',  dateOfBirth: '2000-01-01', age: 20},
-  {name: 'Lora Bay',  dateOfBirth: '1977-03-03', age: 43},
+  {"name": "John Smith", "occupation": "Advisor", "dateOfBirth": "1984-05-05", "age": 36},
+  {"name": "Muhi Masri", "occupation": "Developer", "dateOfBirth": "1992-02-02", "age": 28},
+  {"name": "Peter Adams", "occupation": "HR", "dateOfBirth": "2000-01-01", "age": 20},
+  {"name": "Lora Bay", "occupation": "Marketing", "dateOfBirth": "1977-03-03", "age": 43},
 ];
 
 const USER_SCHEMA = {
-  name: 'text',
-  dateOfBirth: 'date',
-  age: 'number',
+  "name": "text",
+  "occupation": "text",
+  "dateOfBirth": "date",
+  "age": "number",
 }
 
 @Component({
@@ -19,8 +20,7 @@ const USER_SCHEMA = {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  displayedColumns: string[] = ['name', 'dateOfBirth', 'age', '$$edit'];
+  displayedColumns: string[] = ['name', 'occupation', 'dateOfBirth', 'age', '$$edit'];
   dataSource = USER_INFO;
   dataSchema = USER_SCHEMA;
-  isEdit = true;
 }
