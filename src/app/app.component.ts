d2db02e6b14182774e6f1efa154569e3879f53f6
Muhi Masri <muhi@muhimasri.com>
2023-01-21 14:57:26 -0500

    Select all support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component, DefaultIterableDiffer, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { MatTableDataSource } from '@angular/material/table';
-import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
-import { User, UserColumns } from './model/user';
-import { UserService } from './services/user.service';
+import { Component, DefaultIterableDiffer, OnInit } from '@angular/core'
+import { MatDialog } from '@angular/material/dialog'
+import { MatTableDataSource } from '@angular/material/table'
+import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component'
+import { User, UserColumns } from './model/user'
+import { UserService } from './services/user.service'
 
 @Component({
   selector: 'app-root',
@@ -11,27 +11,27 @@ import { UserService } from './services/user.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  displayedColumns: string[] = UserColumns.map((col) => col.key);
-  columnsSchema: any = UserColumns;
-  dataSource = new MatTableDataSource<User>();
-  valid: any = {};
+  displayedColumns: string[] = UserColumns.map((col) => col.key)
+  columnsSchema: any = UserColumns
+  dataSource = new MatTableDataSource<User>()
+  valid: any = {}
 
   constructor(public dialog: MatDialog, private userService: UserService) {}
 
   ngOnInit() {
     this.userService.getUsers().subscribe((res: any) => {
-      this.dataSource.data = res;
-    });
+      this.dataSource.data = res
+    })
   }
 
   editRow(row: User) {
     if (row.id === 0) {
       this.userService.addUser(row).subscribe((newUser: User) => {
-        row.id = newUser.id;
-        row.isEdit = false;
-      });
+        row.id = newUser.id
+        row.isEdit = false
+      })
     } else {
-      this.userService.updateUser(row).subscribe(() => (row.isEdit = false));
+      this.userService.updateUser(row).subscribe(() => (row.isEdit = false))
     }
   }
 
@@ -44,20 +44,20 @@ export class AppComponent {
       birthDate: '',
       isEdit: true,
       isSelected: false,
-    };
-    this.dataSource.data = [newRow, ...this.dataSource.data];
+    }
+    this.dataSource.data = [newRow, ...this.dataSource.data]
   }
 
   removeRow(id: number) {
     this.userService.deleteUser(id).subscribe(() => {
       this.dataSource.data = this.dataSource.data.filter(
-        (u: User) => u.id !== id
-      );
-    });
+        (u: User) => u.id !== id,
+      )
+    })
   }
 
   removeSelectedRows() {
-    const users = this.dataSource.data.filter((u: User) => u.isSelected);
+    const users = this.dataSource.data.filter((u: User) => u.isSelected)
     this.dialog
       .open(ConfirmDialogComponent)
       .afterClosed()
@@ -65,24 +65,39 @@ export class AppComponent {
         if (confirm) {
           this.userService.deleteUsers(users).subscribe(() => {
             this.dataSource.data = this.dataSource.data.filter(
-              (u: User) => !u.isSelected
-            );
-          });
+              (u: User) => !u.isSelected,
+            )
+          })
         }
-      });
+      })
   }
 
   inputHandler(e: any, id: number, key: string) {
     if (!this.valid[id]) {
-      this.valid[id] = {};
+      this.valid[id] = {}
     }
-    this.valid[id][key] = e.target.validity.valid;
+    this.valid[id][key] = e.target.validity.valid
   }
 
   disableSubmit(id: number) {
     if (this.valid[id]) {
-      return Object.values(this.valid[id]).some((item) => item === false);
+      return Object.values(this.valid[id]).some((item) => item === false)
     }
-    return false;
+    return false
+  }
+
+  isAllSelected() {
+    return this.dataSource.data.every((item) => item.isSelected)
+  }
+
+  isAnySelected() {
+    return this.dataSource.data.some((item) => item.isSelected)
+  }
+
+  selectAll(event: any) {
+    this.dataSource.data = this.dataSource.data.map((item) => ({
+      ...item,
+      isSelected: event.checked,
+    }))
   }
 }
